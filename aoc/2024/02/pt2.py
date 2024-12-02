e6aeeab2352e8cfae472caeb741b1db1596e0fee
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-12-02 10:11:48 -0500

    use reduce

--- aoc/2024/02/pt2.py
+++ aoc/2024/02/pt2.py
@@ -1,25 +1,21 @@
 from itertools import pairwise
-
+from functools import reduce
 from aoc import read
 
 
 def main():
   reports = read(lambda x: list(map(int, x.split())))
-  def test(levels):
+  def test(acc, levels):
     for i in range(len(levels)):
       tmp = levels[:i] + levels[i+1:]
       diffs = [y - x for x, y in pairwise(tmp)]
       asc = [0 < d < 4 for d in diffs]
       desc = [-4 < d < 0 for d in diffs]
       if all(asc) or all(desc):
-         return True
-    return False
+         return acc + 1
+    return acc
 
-  return len([
-    levels
-    for levels in reports
-    if test(levels)
-  ])
+  return reduce(test, reports, 0)
 
 if __name__ == '__main__':
     print(main())
