965ac6acfce03040d7eb3614699c9bd34de22800
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-12-02 09:12:39 -0500

    simplified

--- aoc/2024/02/pt2.py
+++ aoc/2024/02/pt2.py
@@ -5,25 +5,22 @@ from aoc import read
 
 def main():
   reports = read(lambda x: list(map(int, x.split())))
-  total = 0
   def test(levels):
-    diffs = [y - x for x, y in pairwise(levels)]
-    asc = [0 < d < 4 for d in diffs]
-    desc = [-4 < d < 0 for d in diffs]
-    return all(asc) or all(desc)
+    length = len(levels)
+    for i in range(length):
+      tmp = levels[0:i] + levels[i+1:length]
+      diffs = [y - x for x, y in pairwise(tmp)]
+      asc = [0 < d < 4 for d in diffs]
+      desc = [-4 < d < 0 for d in diffs]
+      if all(asc) or all(desc):
+         return True
+    return False
 
-  for levels in reports:
-    if test(levels):
-      total += 1
-    else:
-      length = len(levels)
-      for i in range(length):
-        if test(levels[0:i] + levels[i+1:length]):
-          total += 1
-          break
-      
-    
-  return total
+  return len([
+    levels
+    for levels in reports
+    if test(levels)
+  ])
 
 if __name__ == '__main__':
     print(main())
