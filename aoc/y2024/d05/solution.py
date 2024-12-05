2420c7051ad9f2c89283592762d705ebcf0bb140
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-12-05 18:48:28 -0500

    day 5

--- aoc/y2024/d05/solution.py
+++ aoc/y2024/d05/solution.py
@@ -20,7 +20,7 @@ def valid(update, rule):
   if x in update and y in update:
     a, b = [update.index(el) for el in rule]
     return a < b
-  return False
+  return True
 
 
 def pt1():
@@ -34,7 +34,6 @@ def pt1():
     ]
     if all(valid(update, rule) for rule in update_rules):
       middles.append(int(update[len(update) // 2]))
-  s = sum(middles)
   return sum(middles)
 
 
@@ -44,12 +43,15 @@ def pt2():
   middles = {}
   def reorder(update, rules, remaining):
     if not remaining and all(valid(update, rule) for rule in rules):
-      middles[str(sorted(update))] = int(update[len(update)//2])
-    for j, val in enumerate(remaining):
-      for i in range(len(update) + 1):
-        update.insert(i, val)
-        reorder(update, rules, remaining[j+1:])
-        update.pop(i)
+      mid = int(update[len(update)//2])
+      print(','.join(update))
+      middles[str(sorted(update))] = mid
+    elif all(valid(update, rule) for rule in rules) and str(sorted(update+remaining)) not in middles:
+      for j, val in enumerate(remaining):
+        for i in range(len(update) + 1):
+          update.insert(i, val)
+          reorder(update, rules, remaining[:j] + remaining[j+1:])
+          update.pop(i)
 
   for update in updates:
     update_rules = [
@@ -59,17 +61,7 @@ def pt2():
     ]
     statuses = [valid(update, rule) for rule in update_rules]
     if not all(statuses):
-      invalid_rules = [update_rules[i] for i, status in enumerate(statuses) if not status]
-      invalids = set()
-      for a, b in invalid_rules:
-        if a in update:
-          update.remove(a)
-        if b in update:
-          update.remove(b)
-        invalids.add(a)
-        invalids.add(b)
-
-      reorder(update, update_rules, list(invalids))
+      reorder([], update_rules, update)
 
   return sum(middles.values())
 
