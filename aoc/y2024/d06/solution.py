69f0682b3394bbfdc6cf33670d061cc31d218847
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-12-06 10:39:48 -0500

    cleanup

--- aoc/y2024/d06/solution.py
+++ aoc/y2024/d06/solution.py
@@ -1,73 +1,67 @@
 from aoc import read
 from copy import deepcopy
+from itertools import cycle
 
-lines = list(read())
-width = len(lines[0])
-board = [c for l in lines for c in list(l)]
-start = board.index('^')
-directions = ['up', 'right', 'down', 'left']
+
+LINES = list(read())
+WIDTH = len(LINES[0])
+BOARD = [c for l in LINES for c in list(l)]
+START = BOARD.index('^')
+DIRECTIONS = ['up', 'right', 'down', 'left']
 
 
 nxt = {
-  'up': lambda p: p - width if p - width > 0 else None,
-  'right': lambda p: p + 1 if (p + 1) % width > 0 else None,
-  'down': lambda p: p + width if p + width < len(board) else None,
-  'left': lambda p: p - 1 if p % width > 0 else None,
+  'up': lambda p: p - WIDTH if p - WIDTH > 0 else None,
+  'right': lambda p: p + 1 if (p + 1) % WIDTH > 0 else None,
+  'down': lambda p: p + WIDTH if p + WIDTH < len(BOARD) else None,
+  'left': lambda p: p - 1 if p % WIDTH > 0 else None,
 }
 
 
 def navigate(board):
-  global start, nxt, direction, directions
   board = deepcopy(board)
-  p = start
-  direction = 0
+  p = START
+  dirs = cycle(DIRECTIONS)
+  direction = next(dirs)
   while True:
     board[p] = 'X'
-    n = nxt[directions[direction]](p)
+    n = nxt[direction](p)
     if n is None:
       return board
     elif board[n] == '#':
-      direction = (direction + 1) % len(directions)
+      direction = next(dirs)
     else:
       p = n
 
 def pt1():
-  global board
-  return navigate(board).count('X')
+  return navigate(BOARD).count('X')
 
 
 def is_loop(are, board):
-  global start, nxt, direction, directions
   board = deepcopy(board)
-  p = start
-  direction = 0
-  board[are] = 'O'
+  p = START
+  dirs = cycle(DIRECTIONS)
+  direction = next(dirs)
   seen_are = False
   memory = set()
   while True:
-    n = nxt[directions[direction]](p)
+    n = nxt[direction](p)
     if (p, n) in memory:
       return 1
     if n is None:
       return 0
     elif board[n] == '#' or n == are:
-      direction = (direction + 1) % len(directions)
-      board[p] = '+'
-
+      direction = next(dirs)
       if seen_are:
         memory.add((p, n))
     else:
       p = n
-      board[p] = str(direction)
     if n == are:
       seen_are = True
 
 def pt2():
-  global start, board
-  ares = [i for i, c in enumerate(navigate(board)) if c == 'X' and c != start]
-
-  s = sum(is_loop(are, board) for are in ares)
-  return s
+  ares = [i for i, c in enumerate(navigate(BOARD)) if c == 'X' and i != START]
+  return sum(is_loop(are, BOARD) for are in ares)
 
 
 def test_pt1():
